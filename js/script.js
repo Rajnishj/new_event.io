1776a123d0ffbca160f9214acfb1300c8cd2052c
Rajnishj <Rajnishj@github.com>
2018-01-05 16:38:16 +0530

    New Event

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+   $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+ });
+
+  var owl = $("#owl-speakers");
+    owl.owlCarousel({
+      autoPlay: 4000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  
+
+  new WOW({ mobile: false }).init();
+});
